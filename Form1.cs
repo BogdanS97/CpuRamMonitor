e5e3293e6ac6c4297ca5562ee81774e4019b287a
unknown <bogdan.stana97@e-uvt.ro>
2020-09-30 22:34:05 +0300

    first commit

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CpuRam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            float fcpu = CPU.NextValue();
+            float dram = RAM.NextValue();
+
+            circularProgressBar1.Value = (int)fcpu;
+            circularProgressBar1.Text = string.Format("{0:0.00}%", fcpu);
+
+            circularProgressBar2.Value = (int)dram;
+            circularProgressBar2.Text = string.Format("{0:0.00}%", dram);
+        }
+    }
+}
